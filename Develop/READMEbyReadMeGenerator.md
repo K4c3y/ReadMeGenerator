6b6d4d0aa4231d5eacbd5a517b4a0fcf605f581d
Kacey Churchill <kaceychurchill@Kaceys-Air-3.lan>
2020-04-16 19:02:08 -0400

    finished ReadMe Generator

---
+++ Develop/READMEbyReadMeGenerator.md
@@ -0,0 +1,36 @@
+
+    ## ReadMe Generator
+    ![](https://img.shields.io/github/followers/k4c3y?style=social)
+    ## Description
+    Create the best readMe files for your Git Repos! 
+    ## Table of Contents
+    1. [Installation](#Installation)
+    2. [Usage](#Usage)
+    3. [License](#License)
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests)
+    6. [Questions](#Questions)
+    ## Installation
+    1. Pull Repo from GitHub 2. Init Node 3. Install all npms with npm install REQUIRED NPMS - npm i axios , npm i inquirer 
+    ## Usage
+    1. Open Terminal 2. Init Node 3. Run program by entering node index 4. Follow the Prompts and let the tool do the rest. 
+    ## License
+    Licensed under Apache
+    ## Contributing
+    https://www.npmjs.com/package/inquirer https://www.npmjs.com/package/axios https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA https://stackedit.io/app# https://nodejs.org/en/docs/
+    ## Test
+    No Examples at this time. 
+    ##[Questions](#Questions)
+    ## Installation
+    1. Pull Repo from GitHub 2. Init Node 3. Install all npms with npm install REQUIRED NPMS - npm i axios , npm i inquirer 
+    ## Usage
+    1. Open Terminal 2. Init Node 3. Run program by entering node index 4. Follow the Prompts and let the tool do the rest. 
+    ## License
+    Licensed under 
+    Apache
+    ## Contributing
+    https://www.npmjs.com/package/inquirer https://www.npmjs.com/package/axios https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA https://stackedit.io/app# https://nodejs.org/en/docs/
+    ## Tests
+    No Examples at this time. 
+    ## Email: null
+    [Profile Image](https://avatars2.githubusercontent.com/u/60406150?v=4)
--- Develop/index.js
+++ Develop/index.js
@@ -1,36 +1,29 @@
 // What to create to accomplish Homework
-
-// Step 1 ask questions to the user 
-
-// step 2 save the answers in variables 
-
-// step 3 create the new file 
-
-// step 4 write the answers to the file 
-
-// upload file to github.com 
-
+// step 1 ask questions   
+// Step 2 store answers from questions  
+// step 3 get github information 
+// step 4 Create and write the file  
 //---------------------------------------------------------
 
 
 // Setting Vairables
-//----------------------------------------------------------
 
-// To access in fs module as promise
+// pulling in npm mods and fs
 const fs = require('fs').promises
-
-// To access the axios module
 const axios = require('axios')
-
-// To access inquirer module
 const inquirer = require('inquirer')
 
 
+// variable for github profile image, email, username, and badge image. 
+let githubPic = ''
+let githubEmail = ''
+let githubUsername = ''
+let badgeImage = ''
 
 
+// Step 1 ask questions to the user 
 
 // questions to pass through inquirer
-// Default = values if user doesn't enter anything 
 const questions = [{
         message: 'Please title your project?',
         name: 'title',
@@ -43,13 +36,6 @@ const questions = [{
         name: 'description',
         type: 'input',
         default: 'Description Default Text, add a description!'
-
-    },
-    {
-        message: 'Add a Table of Contents',
-        name: 'table',
-        type: 'input',
-        default: 'Table of Contents'
 
     },
     {
@@ -70,8 +56,8 @@ const questions = [{
         message: 'Pick a license',
         name: 'license',
         type: 'list',
-        choices: ['Apache', 'EPL', 'MIT']
-        default: 'Apache',
+        choices: ['Apache', 'Bower', 'CTAN', 'DUB', 'EPL', 'MIT', 'APM', ]
+       
 
     },
     {
@@ -89,4 +75,104 @@ const questions = [{
 
     }
 ]
+
+// still step 1 asks the user the questions for readme 
+function promptQuestions() {
+    return inquirer.prompt(questions)
+}
+
+
+ // step 2 save the answers in variables  
+// takes user input answers and formats readme file 
+function createReadMe(answers) {
+  
+   
+    return `
+    ## ${answers.title}
+    ![](${badgeImage})
+    ## Description
+    ${answers.description}
+    ## Table of Contents
+    1. [Installation](#Installation)
+    2. [Usage](#Usage)
+    3. [License](#License)
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests)
+    6. [Questions](#Questions)
+    ## Installation
+    ${answers.installation}
+    ## Usage
+    ${answers.usage}
+    ## License
+    Licensed under ${answers.license}
+    ## Contributing
+    ${answers.contributing}
+    ## Test
+    ${answers.tests}
+    ##[Questions](#Questions)
+    ## Installation
+    ${answers.installation}
+    ## Usage
+    ${answers.usage}
+    ## License
+    Licensed under 
+    ${answers.license}
+    ## Contributing
+    ${answers.contributing}
+    ## Tests
+    ${answers.tests}
+    ## Email: ${githubEmail}
+    [Profile Image](${githubPic})`
+}
+
+// step 3 ask for github username
+function promptUsername() {
+    return inquirer.prompt({
+        message: 'Enter Github username?',
+        name: 'username'
+    })
+}
+// stores github username, email, picture, and loads followers based on user name. 
+async function getGithubUsername({ username }) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${username}`)
+            // stores the image and email and username
+        githubPic = response.data.avatar_url
+        githubEmail = response.data.email
+        githubUsername = username
+        badgeImage = `https://img.shields.io/github/followers/${githubUsername}?style=social`
+
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// step 4 create and write the answers to the file 
+async function writeReadMe(fileContent) {
+    try {
+        await fs.writeFile('READMEbyReadMeGenerator.md', fileContent)
+
+    } catch (err) {
+        console.log(err)
+    }
+
+ console.log('Success!')
+}
+
+
+// calling functions 
+
+// Ask for github username
+promptUsername()
+    // gets profile image with the axios module
+    .then(getGithubUsername)
+    // queque questions for readme file 
+    .then(promptQuestions)
+    // creates the readme file 
+    .then(createReadMe)
+    // writes the answers from the questions to the read me file 
+    .then(writeReadMe)
+
+
 
