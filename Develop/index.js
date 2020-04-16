e31506f05f9cff1b84680224f2fd2d7df582663a
Kacey Churchill <kaceychurchill@Kaceys-Air-3.lan>
2020-04-16 15:09:51 -0400

    added question prompts

---
+++ Develop/index.js
@@ -0,0 +1,93 @@
+// What to create to accomplish Homework
+
+// Step 1 ask questions to the user 
+
+// step 2 save the answers in variables 
+
+// step 3 create the new file 
+
+// step 4 write the answers to the file 
+
+// upload file to github.com 
+
+//---------------------------------------------------------
+
+
+// Setting Vairables
+//----------------------------------------------------------
+
+// To access in fs module as promise
+const fs = require('fs').promises
+
+// To access the axios module
+const axios = require('axios')
+
+// To access inquirer module
+const inquirer = require('inquirer')
+
+
+// Define a global variable for the avatar URL
+let avatarURL = ''
+
+
+// questions to pass through inquirer
+// Default = values if user doesn't enter anything 
+const questions = [{
+        message: 'Please title your project?',
+        name: 'title',
+        type: 'input',
+        default: 'Title'
+
+    },
+    {
+        message: 'Enter a description for the project',
+        name: 'description',
+        type: 'input',
+        default: 'Description Default Text, add a description!'
+
+    },
+    {
+        message: 'Add a Table of Contents',
+        name: 'table',
+        type: 'input',
+        default: 'Table of Contents'
+
+    },
+    {
+        message: 'Add a installation guide for your project',
+        name: 'installation',
+        type: 'input',
+        default: 'Installation Guide'
+
+    },
+    {
+        message: 'Add instructions on how to use',
+        name: 'usage',
+        type: 'input',
+        default: 'Usage Guide'
+
+    },
+    {
+        message: 'Pick a license',
+        name: 'license',
+        type: 'list',
+        choices: ['Apache', 'EPL', 'MIT']
+        default: 'Apache',
+
+    },
+    {
+        message: 'Add contributing Guide',
+        name: 'contributing',
+        type: 'input',
+        default: 'Contributing Guide',
+
+    },
+    {
+        message: 'Add tests examples with instructions',
+        name: 'tests',
+        type: 'input',
+        default: 'Test examples'
+
+    }
+]
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# ReadMeGenerator
-Use this tool to always build a nice read me document for your Github projects. 
